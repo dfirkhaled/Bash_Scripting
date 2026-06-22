595cde5a106e5efe465958a79ba9cca844e8f7e7
dfirkhaled <dfir.khaled@gmail.com>
2026-06-22 13:06:54 +0300

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+name="Khaled"
+age=18
+dep="Cybersecurity"
+CurrD=$(pwd)
+Date=$(date)
+
+echo "Hello, My name is $name."
+echo "I'm $age Years old."
+echo "Current Working Directory: $CurrD"
+echo "The Date Is: $Date"
+
